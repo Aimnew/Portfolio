58f621a1eb5295e52e8b3a8d79e2820462c22f7e
Aimnew <aicoolgo@yandex.ru>
2025-07-23 23:10:49 +0300

    Добавил структуру портфолио

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,44 +1,32 @@
 // components/ui/badge.tsx
 import { cn } from "@/lib/utils";
-import { VariantProps, cva } from "class-variance-authority";
-import { HTMLAttributes, forwardRef } from "react";
+import { HTMLAttributes } from "react";
 
-const badgeVariants = cva(
-  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
-  {
-    variants: {
-      variant: {
-        default:
-          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
-        secondary:
-          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        destructive:
-          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
-        outline: "text-foreground border-border",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-    },
-  }
-);
+interface BadgeProps extends HTMLAttributes<HTMLSpanElement> {
+  variant?: "default" | "secondary" | "outline" | "destructive";
+}
 
-export interface BadgeProps
-  extends HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof badgeVariants> {}
-
-const Badge = forwardRef<HTMLDivElement, BadgeProps>(
-  ({ className, variant, ...props }, ref) => {
-    return (
-      <div
-        ref={ref}
-        className={cn(badgeVariants({ variant }), className)}
-        {...props}
-      />
-    );
-  }
-);
-
-Badge.displayName = "Badge";
-
-export { Badge, badgeVariants };
+export function Badge({
+  className,
+  variant = "default",
+  ...props
+}: BadgeProps) {
+  return (
+    <span
+      className={cn(
+        "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+        {
+          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80":
+            variant === "default",
+          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80":
+            variant === "secondary",
+          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80":
+            variant === "destructive",
+          "text-foreground border-border": variant === "outline",
+        },
+        className
+      )}
+      {...props}
+    />
+  );
+}
