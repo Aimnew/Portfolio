2de31bfe43c50ec6ec20a3cefca46fb3d3885b41
Aimnew <aicoolgo@yandex.ru>
2025-07-23 22:55:06 +0300

    Добавил структуру портфолио

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,41 +1,30 @@
 import { cn } from "@/lib/utils";
-import { VariantProps, cva } from "class-variance-authority";
-import { HTMLAttributes, forwardRef } from "react";
+import { HTMLAttributes } from "react";
 
-const badgeVariants = cva(
-  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
-  {
-    variants: {
-      variant: {
-        default: "border-transparent bg-primary text-primary-foreground",
-        secondary: "border-transparent bg-secondary text-secondary-foreground",
-        destructive:
-          "border-transparent bg-destructive text-destructive-foreground",
-        outline: "text-foreground",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-    },
-  }
-);
+interface BadgeProps extends HTMLAttributes<HTMLSpanElement> {
+  variant?: "default" | "secondary" | "outline";
+}
 
-export interface BadgeProps
-  extends HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof badgeVariants> {}
-
-const Badge = forwardRef<HTMLDivElement, BadgeProps>(
-  ({ className, variant, ...props }, ref) => {
-    return (
-      <div
-        ref={ref}
-        className={cn(badgeVariants({ variant }), className)}
-        {...props}
-      />
-    );
-  }
-);
-
-Badge.displayName = "Badge";
-
-export { Badge, badgeVariants };
+export function Badge({
+  className,
+  variant = "default",
+  ...props
+}: BadgeProps) {
+  return (
+    <span
+      className={cn(
+        "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-medium",
+        {
+          "border-transparent bg-primary text-primary-foreground":
+            variant === "default",
+          "border-transparent bg-secondary text-secondary-foreground":
+            variant === "secondary",
+          "border-gray-200 bg-transparent text-gray-800 dark:border-gray-700 dark:text-gray-200":
+            variant === "outline",
+        },
+        className
+      )}
+      {...props}
+    />
+  );
+}
