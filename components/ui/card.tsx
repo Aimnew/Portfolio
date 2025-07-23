8b5981f484921592378f4a85000a824ab4e422a9
Aimnew <aicoolgo@yandex.ru>
2025-07-23 23:23:30 +0300

    Добавил структуру портфолио

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,8 +1,10 @@
+// components/ui/card.tsx
 "use client";
-import * as React from "react";
 
+import * as React from "react";
 import { cn } from "@/lib/utils";
 
+// Определение типа для доступных размеров, если нужно
 const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
@@ -10,13 +12,25 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-xl border bg-card text-card-foreground shadow",
       className
     )}
     {...props}
   />
 ));
 Card.displayName = "Card";
+
+const CardHeader = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("flex flex-col space-y-1.5 p-6", className)}
+    {...props}
+  />
+));
+CardHeader.displayName = "CardHeader";
 
 const CardContent = React.forwardRef<
   HTMLDivElement,
@@ -26,4 +40,17 @@ const CardContent = React.forwardRef<
 ));
 CardContent.displayName = "CardContent";
 
-export { Card, CardContent };
+const CardFooter = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("flex items-center p-6 pt-0", className)}
+    {...props}
+  />
+));
+CardFooter.displayName = "CardFooter";
+
+// Экспортируем все необходимые компоненты
+export { Card, CardHeader, CardContent, CardFooter };
