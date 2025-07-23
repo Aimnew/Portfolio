214dfa4d4e469ff7cd085edf2367d2505990f1bc
Aimnew <aicoolgo@yandex.ru>
2025-07-24 00:41:17 +0300

    Добавил структуру портфолио

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["t.me", "*.streamlit.app"],
+    domains: ["t.me", "*.streamlit.app", "github.com"],
   },
+  output: process.env.NEXT_OUTPUT_MODE === "export" ? "export" : undefined,
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
